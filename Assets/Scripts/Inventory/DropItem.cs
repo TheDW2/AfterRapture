44e60d5c700a36a96208f5a208f2ba20eb4a880d
Josh <139287882+bombwastakenn@users.noreply.github.com>
2023-07-13 19:24:36 -0400

    Torch light
    changing art later (when they're done)

--- Assets/Scripts/Inventory/DropItem.cs
+++ Assets/Scripts/Inventory/DropItem.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DropItem : MonoBehaviour
 {
-    public GameObject mainCamera;
+    
     public CollectiblesList c;
     public GameObject gameObjects;
 
@@ -14,7 +14,7 @@ public class DropItem : MonoBehaviour
     void Update() {
         if (InvManager.holding > 0 && Input.GetKeyDown(KeyCode.Q)) {
             holding = c.collectableItems[InvManager.holding];
-            GameObject temp = Instantiate(holding, new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, 0), holding.transform.rotation);
+            GameObject temp = Instantiate(holding, new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0), holding.transform.rotation);
             temp.transform.parent = gameObjects.transform;
             temp.name = holding.name;
             InvManager.inventory[InvManager.onSlot] = 0;

6979f5c4658fb4a2f7f01b62f4ff8ccc82c66a65
Kathleen <kathy.febiola02@gmail.com>
2023-07-14 23:55:54 +0700

    fix characterstory

--- Assets/Scripts/Model/Character/CharacterStory.cs
+++ Assets/Scripts/Model/Character/CharacterStory.cs
@@ -7,4 +7,7 @@ public class CharacterStory
 {
     //put story object here
     public Location _storyLocation;
+    public bool _finished;
+    public bool presequite_decision; //if the story depends on what decision has been made before (it can be bool or int or other type depending on the complexity of the decision)
+    public bool final_decision; //what decision is made in the story (it can be bool or int or other type depending on the complexity of the decision)
 }

a5f8e50018d0eda560fe03a27bae49c8d08d1d1f
Kathleen <kathy.febiola02@gmail.com>
2023-07-14 23:22:39 +0700

    add new scripts

---
+++ Assets/Scripts/Model/Inventory/InventoryItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class InventoryItem 
+{
+    public int id;
+    public string name;
+    public int amount;
+}

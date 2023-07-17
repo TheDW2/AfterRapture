8bcb37a67694cbec1ebd27db55a4d09a67fbb4f2
Josh <139287882+bombwastakenn@users.noreply.github.com>
2023-07-17 12:12:57 -0400

    Revert "Revert "Revert "Player's apartment"""
    This reverts commit f47bfec4241cc8c35aff1d21baa0b76822056671.

--- Assets/Scripts/Inventory/Collectibles.cs
+++ Assets/Scripts/Inventory/Collectibles.cs
@@ -13,20 +13,11 @@ public class Collectibles : MonoBehaviour
     
     //OnMouseDrag is basically just when it's clicked, right?
     void OnMouseDrag() {
-        Location l = ScriptableObject.CreateInstance<Location>();
-
         //Check if at least one slot is empty
         if (InvManager.inventory.Contains(0)) {
-            //If so, check if the player is at home
-            if (l._locationId == 0) {
-                //If so, add the item to the inventory
-                InvManager.AddItem(textureID);
-                Destroy(this.gameObject);
-
-            } else {
-                //Else, don't add the item to the inventory
-                invFull = true;
-            }
+            //If so, add the item to the inventory
+            InvManager.AddItem(textureID);
+            Destroy(this.gameObject);
         } else {
             //That means the inventory is full. Show a message to the user that the inventory is full
            invFull = true;

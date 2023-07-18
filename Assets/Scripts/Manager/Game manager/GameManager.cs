ab4fc4a38c474ddf27040c6f0c7cc5612fe73e33
Kathleen <kathy.febiola02@gmail.com>
2023-07-18 22:55:22 +0700

    Toggle day and night + day counter

--- Assets/Scripts/Manager/Game manager/GameManager.cs
+++ Assets/Scripts/Manager/Game manager/GameManager.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-   //all game management goes here
+   [SerializeField] private Button _toggleCycle;
+   private SaveFile _saveFile;
+
+   void Start()
+   {
+      _saveFile = SaveHandler.instance.LoadSlot(PlayerPrefs.GetInt("current_slot_used"));
+      _toggleCycle.onClick.AddListener(ToggleCycle);
+   }
+
+   private void ToggleCycle()
+   {
+      if(_saveFile._playerSave._timeCycle == TimeCycle.Day)
+      {
+         _saveFile._playerSave._timeCycle = TimeCycle.Night;
+      }
+
+      else
+      {
+         _saveFile._playerSave._timeCycle = TimeCycle.Day;
+         _saveFile._playerSave.dayCount += 1;
+      }
+
+      SaveHandler.instance.SaveSlot(_saveFile, PlayerPrefs.GetInt("current_slot_used"));
+      SceneManagerHandler.instance.LoadScene(2);
+   }
 }

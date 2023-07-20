7457b2d10eb4ea8ef7c63cd753e2cd97ca51a169
Josh <139287882+bombwastakenn@users.noreply.github.com>
2023-07-20 13:22:34 -0400

    Fixed prefab issue
    fixed the prefab issue with instantiating the apartment gameobject

--- Assets/Scripts/Objects/SpecialItems.cs
+++ Assets/Scripts/Objects/SpecialItems.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class SpecialItems : MonoBehaviour
 {
-    //This script holds special items
-    public UnityEngine.Rendering.Universal.Light2D lightSource;
-
-    public static Vector3 camPos;
-
-    void Update() {
-        camPos = Camera.main.transform.position;
-        Torch();
-    }
     
-    void Torch() {
-        //Disable GlobalLight and Background for normal view
-        
-        if (InvManager.holding == 3) {
-            lightSource.enabled = true;
-        } else {
-            lightSource.enabled = false;
-        }
-    }
 }

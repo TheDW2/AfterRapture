215da9a119b01c4c8c9962a4495b34f23fb330f4
Maddie Grey <madelinedg@gmail.com>
2023-07-22 17:39:04 -0400

    adjusted music controllers

--- Assets/Scripts/Audio/MusicController.cs
+++ Assets/Scripts/Audio/MusicController.cs
@@ -5,19 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class MusicController : MonoBehaviour
 {
-
+    private FMOD.Studio.EventInstance instance;
     // Start is called before the first frame update
     void Start()
     {
+        instance.release();
+        instance = FMODUnity.RuntimeManager.CreateInstance("event:/Music/BG Music");
         // Get the active scene
         Scene scene = SceneManager.GetActiveScene();
         Debug.Log(scene.name);
+        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Time Of Day", 0);
 
         // Switch initial music depending on scene
         switch (scene.name)
         {
-            case "0. Mainmenu":
-                FMODUnity.RuntimeManager.PlayOneShot("event:/Music/Menu");
+            case "2. MapScene":
+                FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Location", 0);
+                instance.start();
+                break;
+            case "3. LocationScene":
+                FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Location", 1);
+                instance.start();
                 break;
         }
     }

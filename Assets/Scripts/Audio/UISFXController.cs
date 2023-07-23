1efc658a2dc68ac2527c83a8b3afb87d2cde5f91
Maddie Grey <madelinedg@gmail.com>
2023-07-23 19:15:23 -0400

    maybe more sfx

--- Assets/Scripts/Audio/UISFXController.cs
+++ Assets/Scripts/Audio/UISFXController.cs
@@ -6,10 +6,12 @@ using UnityEngine.UI;
 public class UISFXController : MonoBehaviour
 {
     private FMOD.Studio.EventInstance _confirmEvent;
+    private FMOD.Studio.EventInstance _pauseEvent;
     // Start is called before the first frame update
     void Start()
     {
         _confirmEvent = FMODUnity.RuntimeManager.CreateInstance("event:/UI/Confirm");
+        _pauseEvent = FMODUnity.RuntimeManager.CreateInstance("event:/UI/Pause");
     }
 
     // Update is called once per frame
@@ -21,5 +23,10 @@ public class UISFXController : MonoBehaviour
     public void OnClick()
     {
         _confirmEvent.start();
+    }
+
+    public void OnPauseClick()
+    {
+        _pauseEvent.start();
     }
 }

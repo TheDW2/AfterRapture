2b61b4a0bec97f82b775957774c92f6f0d6880cf
Maddie Grey <madelinedg@gmail.com>
2023-07-22 20:04:43 -0400

    fixing music system

--- Assets/Scripts/Audio/MusicController.cs
+++ Assets/Scripts/Audio/MusicController.cs
@@ -5,34 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class MusicController : MonoBehaviour
 {
-    private FMOD.Studio.EventInstance instance;
-    // Start is called before the first frame update
+    private FMOD.Studio.EventInstance _musicEvent;
+    private FMOD.Studio.EventInstance _mapMusicEvent;
+ 
+   
     void Start()
     {
-        instance.release();
-        instance = FMODUnity.RuntimeManager.CreateInstance("event:/Music/BG Music");
-        // Get the active scene
-        Scene scene = SceneManager.GetActiveScene();
-        Debug.Log(scene.name);
-        FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Time Of Day", 0);
+        DontDestroyOnLoad(this);
+        _musicEvent = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Menu");
+        _mapMusicEvent = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Map Music");
+        _musicEvent.start();
+        //_musicEvent.getParameter("Exit", out _exitParameter);
 
-        // Switch initial music depending on scene
-        switch (scene.name)
-        {
-            case "2. MapScene":
-                FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Location", 0);
-                instance.start();
-                break;
-            case "3. LocationScene":
-                FMODUnity.RuntimeManager.StudioSystem.setParameterByName("Location", 1);
-                instance.start();
-                break;
-        }
+    }
+
+    void Awake()
+    {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ((SceneManager.GetActiveScene().name == "2. MapScene") && (_musicEvent.isValid() == true))
+        {
+            //Debug.Log("I am changing a parameter!!!!");
+
+
+            _musicEvent.setParameterByName("MenuToMap", 1);
+            //Debug.Log(_musicEvent.setParameterByName("MenuToMap", 1));
+            //Debug.Log(_musicEvent.isValid());
+        } else if (SceneManager.GetActiveScene().name == "2. MapScene")
+        {
+            _mapMusicEvent.start();
+            Debug.Log("map music should start");
+        }
+
+        if (SceneManager.GetActiveScene().name == "3. LocationScene")
+        {
+            _musicEvent.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            //Destroy(this.gameObject);
+            //Debug.Log("I am being destroyed?");
+            
+        }
     }
 }
